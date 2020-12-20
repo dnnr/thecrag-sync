eaea8e41dc8e1faa7de9b79e43cd25c2ecab30aa
Daniel Danner <daniel@danner.de>
2020-12-20 17:39:16 +0100

    Add reading and parsing of logbook file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
 use chrono::NaiveDate;
 use std::fs;
 use structopt::StructOpt;
 use std::io;
 use std::path::PathBuf;
 use std::collections::HashMap;
+use regex::Regex;
 
 #[derive(StructOpt)]
 #[structopt(about = "Compare theCrag CSV export to manually maintained logbook")]
@@ -43,10 +48,10 @@ fn generate_diff(csv_file: &PathBuf, logbook_file: &PathBuf) -> Result<String, i
     let csv_string = fs::read_to_string(csv_file)?;
     let logbook_string = fs::read_to_string(logbook_file)?;
 
-    let csv_ticks = get_ticks_from_csv(&csv_string);
-    let logbook_days = get_logdays_from_logbook(&logbook_string);
+    let csv_ticks = get_ticks_from_csv(&csv_string)?;
+    let logbook_days = get_logdays_from_logbook(&logbook_string)?;
 
-    println!("{:?}", csv_ticks?);
+    println!("{:#?}", csv_ticks);
 
     Ok("No diff to report yet".to_string())
 }
@@ -85,6 +90,44 @@ fn get_ticks_from_csv(csv_string: &str) -> Result<Vec<Tick>, io::Error> {
     Ok(ticks)
 }
 
+fn parse_logbook_line(line: &str) -> Result<Option<LogDay>, io::Error> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^([0-9-]+): Felsklettern \(([^()]+)\)").unwrap();
+    }
+
+    let captures = match RE.captures(line) {
+        Some(captures) => captures,
+        None => return Ok(None)
+    };
+    let date_str = captures[1].to_string();
+    let crag_name = captures[2].to_string();
+
+    let date = match NaiveDate::parse_from_str(&date_str, "%Y-%m-%d") {
+        Ok(date) => date,
+        Err(err) => {
+            return Err(io::Error::new(io::ErrorKind::Other, format!("Cannot parse logbook date \"{}\": {}", date_str, err)));
+        }
+    };
+
+    Ok(Some(LogDay { crag_name, date }))
+}
+
 fn get_logdays_from_logbook(logbook_string: &str) -> Result<Vec<LogDay>, io::Error> {
+    let logbook_lines = logbook_string.split("\n")
+        .filter(|line| line.len() > 0)
+        .skip_while(|line| *line != "### BEGIN theCrag sync")
+        .skip(1)
+        .collect::<Vec<&str>>();
+
+    let mut logdays: Vec<LogDay> = Vec::new();
+    for line in logbook_lines {
+        match parse_logbook_line(line)? {
+            Some(logday) => logdays.push(logday),
+            None => {},
+        };
+    }
+
+    println!("logdays: {:#?}", logdays);
+
     Ok(vec![])
 }
